50c9ab227152f799a5ea325bb13ab81115b03342
Michael <michael.salr1590@gmail.com>
2018-04-16 16:20:38 +0200

    Initial commit

---
+++ android/src/com/escoladeltreball/wiam2/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.escoladeltreball.wiam2;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.escoladeltreball.wiam2.GameMain;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new GameMain(), config);
+	}
+}
---
+++ core/src/com/escoladeltreball/wiam2/MyPacker.java
@@ -0,0 +1,13 @@
+package com.escoladeltreball.wiam2;
+
+/**
+ * Created by Michael
+ */
+public class MyPacker {
+
+    public static void main(String[] args) throws Exception {
+
+//        TexturePacker.process();
+
+    }
+}
